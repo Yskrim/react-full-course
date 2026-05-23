1cbe18610172460f1f3176320c8a5740c1f60b3b
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-05-23 15:44:23 +1000

    Create multiple pages in one (SPA) using react-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,14 @@
 // import { useState } from 'react'
-import { HomePage } from './components/pages/HomePage'
+import { Route, Routes } from 'react-router'
+import { HomePage } from './components/pages/HomePage' 
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<div>Test checkout page</div>} />
+    </Routes>
   )
 }
 
