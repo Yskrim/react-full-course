0649da03ce92ce14b9af4c921876319f93440d48
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-06-07 20:12:32 +1000

    User Interaction Test for clicking addToCart button

--- ecommerce-project/src/components/elements/Home/ProductContainer.jsx
+++ ecommerce-project/src/components/elements/Home/ProductContainer.jsx
@@ -74,7 +74,7 @@ export function ProductContainer({ product, loadCart }) {
 				Added
 			</div>
 
-			<button className="add-to-cart-button button-primary" onClick={addToCart}>
+			<button className="add-to-cart-button button-primary" onClick={addToCart} data-testid="add-to-cart-button">
 				Add to Cart
 			</button>
 		</div>
