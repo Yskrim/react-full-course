7cd492732393da1b72e96a70bc81cdf8a4dcb4f6
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-06-08 11:24:52 +1000

    9b. add a test case for negative numbers for formatMoney; tweak formatMoney for right output

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -11,4 +11,9 @@ describe("formatMoney", () => {
         expect(formatMoney(100)).toBe("$1.00");
         expect(formatMoney(0)).toBe("$0.00");
     });
+
+    it("displays negative numbers", () => {
+        expect(formatMoney(-999)).toBe("-$9.99");
+        expect(formatMoney(-100)).toBe("-$1.00");
+    })
 });
